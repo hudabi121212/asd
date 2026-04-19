7962a3d12f134a19a02b47706a25574944e6e9de
hudabi121212 <song7222111@naver.com>
2026-04-19 23:38:17 +0900

    Add files via upload

--- miniWebsSite.js
+++ miniWebsSite.js
@@ -3,30 +3,35 @@ const playerBox = document.getElementById("player-box");
 const animeButtons = document.querySelectorAll(".anime-btn"); // All 안붙이면 첫번째 버튼만 작동함 하나만 가져오는거니까 클래스에서 
 const animeList = document.getElementById("anime-list");
 
+
 const animeDate = {
             hyouka:{
                 title: "빙과",
                 episodes: [
-                        {number:1, url:"https://www.youtube.com/watch?v=22-qDRYQqeA&list=PL30412D335B665FAE"},
-                        {number:2, url:"https://youtu.be/ZCiGJKO9I9w?si=Z8bg5QB1QXZn0IwR"},
-                        {number:3, url:"https://youtu.be/5-ZbV04RcaI?si=uyUhAWbIgzikgid7"},
-                        {number:4, url:"https://anilife.app/watch?id=d3df43e5-4b70-4e9b-a8b3-e870f44c9c75"},
-                        {number:5, url:"https://www.youtube.com/watch?v=22-qDRYQqeA&list=PL30412D335B665FAE"},
-                        {number:6, url:"https://www.youtube.com/watch?v=22-qDRYQqeA&list=PL30412D335B665FAE"}
+                        {number:1, url:"https://www.youtube.com/embed/0XWaEBXQyPU"},
+                        {number:2, url:"https://www.youtube.com/watch?v=0XWaEBXQyPU"},
+                        {number:3, url:"https://www.youtube.com/embed/dQw4w9WgXcQ"},
+                        {number:4, url:"https://www.youtube.com/embed/dQw4w9WgXcQ"},
+                        {number:5, url:"https://www.youtube.com/embed/dQw4w9WgXcQ"},
+                        {number:6, url:"https://www.youtube.com/embed/dQw4w9WgXcQ"}
                 ]                
             },
 
             lain: {
                 title: "시리얼익스페리먼트",
                 episodes: [
-                        {number:1, url:"https://www.youtube.com/watch?v=22-qDRYQqeA&list=PL30412D335B665FAE"},
-                        {number:2, url:"https://youtu.be/ZCiGJKO9I9w?si=Z8bg5QB1QXZn0IwR"},
-                        {number:3, url:"https://youtu.be/5-ZbV04RcaI?si=uyUhAWbIgzikgid7"},
-                        {number:4, url:"https://youtu.be/Q3UmWgzpC4w?si=T-j18AAyiKxOKy81"},
+                        {number:1, url:"https://www.youtube.com/embed/dQw4w9WgXcQ"},
+                        {number:2, url:"https://www.youtube.com/embed/dQw4w9WgXcQ"},
+                        {number:3, url:"https://www.youtube.com/embed/dQw4w9WgXcQ"},
+                        {number:4, url:"https://www.youtube.com/embed/dQw4w9WgXcQ"}
+
+                        /*
                         {number:5, url:"https://www.youtube.com/watch?v=22-qDRYQqeA&list=PL30412D335B665FAE"},
                         {number:6, url:"https://www.youtube.com/watch?v=22-qDRYQqeA&list=PL30412D335B665FAE"},
                         {number:7, url:"https://www.youtube.com/watch?v=22-qDRYQqeA&list=PL30412D335B665FAE"},
                         {number:8, url:"https://www.youtube.com/watch?v=22-qDRYQqeA&list=PL30412D335B665FAE"}
+
+                        */
                 ]
             }
  };    // 이게 객체를 만든거임 animeDate는 
@@ -96,15 +101,30 @@ for(let i = 0 ; i < animeButtons.length; i++ )
         {
             const episodeBtn = document.createElement("button");    // 이미 메모리에 HTML객체로 버튼 만든거임
             const episode = selectedAnime.episodes[j];                 // 이건 알지? 객체 만든거임 episode라는 내가 누른버튼에 애니메의 에피소드객체들로
-            episodeBtn.textContent = episode.number + "화";         // 요건 버튼안에 글자넣는거
+            episodeBtn.textContent = episode.number + "화";            // 요건 버튼안에 글자넣는거
             episodeList.appendChild(episodeBtn);
             episodeBtn.classList.add("episode-btn"); // 이거 연결해줘야됌 여기서 episodeBtn은 클래스도 ID도 아닌 변수니까 ㅇㅇ 이러면 이제 클래스가된거임
 
 
-
             episodeBtn.addEventListener("click", function(event) {
                 event.stopPropagation(); // 요게 body클릭 이벤트 막는거임 
-                window.location.href = episode.url; // 이게 객체에있는 url 갖고온거임 
+
+
+                console.log(episode.url);
+
+                playerBox.style.display = "block";    
+                playerBox.innerHTML = `
+                <iframe 
+                    width="560" 
+                    height="315" 
+                    src="${episode.url}" 
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerpolicy="strict-origin-when-cross-origin"
+                    allowfullscreen>
+                </iframe>
+                `;
+                //window.location.href = episode.url; // 이게 객체에있는 url 갖고온거임 
             });
         }
         
